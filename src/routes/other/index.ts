c1a462ccbf47a2dc0c00803ca8b1280d44c8df2b
Rashik Buksh <rashikbuksh@gmail.com>
2025-03-06 17:16:11 +0600

    feat: integrate HR routes and add shop route to portfolio

--- src/routes/other/index.ts
+++ src/routes/other/index.ts
@@ -1,7 +1,9 @@
+import hr from './hr';
 import portfolio from './portfolio';
 
 const other = [
   ...portfolio,
+  ...hr,
 ];
 
 export default other;
--- src/routes/other/portfolio/index.ts
+++ src/routes/other/portfolio/index.ts
@@ -1,4 +1,5 @@
 import product_category from './product_category';
 import product_sub_category from './product_sub_category';
+import shop from './shop';
 
-export default [product_category, product_sub_category];
+export default [product_category, product_sub_category, shop];
