905cd090d5ace52b41ea797bd4b004e287f7fbef
Rashik Buksh <rashikbuksh@gmail.com>
2025-03-09 15:25:45 +0600

    feat: add product route to portfolio index

--- src/routes/other/portfolio/index.ts
+++ src/routes/other/portfolio/index.ts
@@ -1,6 +1,7 @@
+import product from './product';
 import product_category from './product_category';
 import product_sub_category from './product_sub_category';
 import sales_point from './sales_point';
 import shop from './shop';
 
-export default [product_category, product_sub_category, shop, sales_point];
+export default [product_category, product_sub_category, shop, sales_point, product];
