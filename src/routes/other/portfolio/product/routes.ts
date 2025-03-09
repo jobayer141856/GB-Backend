503dd7a3c38fd86cbf598350738b3c9c2d982b9e
Rashik Buksh <rashikbuksh@gmail.com>
2025-03-09 14:35:42 +0600

    feat: add value label route and handler for product portfolio

---
+++ src/routes/other/portfolio/product/routes.ts
@@ -0,0 +1,23 @@
+import * as HSCode from 'stoker/http-status-codes';
+import { jsonContent } from 'stoker/openapi/helpers';
+
+import { createRoute, z } from '@hono/zod-openapi';
+
+const tags = ['other'];
+
+export const valueLabelRoute = createRoute({
+  path: '/other/portfolio/product/value/label',
+  method: 'get',
+  tags,
+  responses: {
+    [HSCode.OK]: jsonContent(
+      z.object({
+        value: z.string(),
+        label: z.string(),
+      }),
+      'The valueLabel of product',
+    ),
+  },
+});
+
+export type ValueLabelRoute = typeof valueLabelRoute;
