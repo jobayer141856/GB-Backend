7e59a484a21f6ec30179edb600876907f2060495
Jobayer Hossen <jobayerhossen64@gmail.com>
2025-03-10 13:39:06 +0600

    feat: enhance value label retrieval by joining product category for improved labeling

--- src/routes/other/portfolio/product_sub_category/handlers.ts
+++ src/routes/other/portfolio/product_sub_category/handlers.ts
@@ -1,18 +1,20 @@
 import type { AppRouteHandler } from '@/lib/types';
 
+import { eq, sql } from 'drizzle-orm';
 import * as HSCode from 'stoker/http-status-codes';
 
 import db from '@/db';
-import { product_sub_category } from '@/routes/portfolio/schema';
+import { product_category, product_sub_category } from '@/routes/portfolio/schema';
 
 import type { ValueLabelRoute } from './routes';
 
 export const valueLabel: AppRouteHandler<ValueLabelRoute> = async (c: any) => {
   const resultPromise = db.select({
     value: product_sub_category.uuid,
-    label: product_sub_category.name,
+    label: sql`${product_sub_category.name} || '-' || ${product_category.name}`,
   })
-    .from(product_sub_category);
+    .from(product_sub_category)
+    .leftJoin(product_category, eq(product_category.uuid, product_sub_category.product_category_uuid));
 
   const data = await resultPromise;
 
