7d08b20ae4b27accb263c9d75cac9550147c8837
Rashik Buksh <rashikbuksh@gmail.com>
2025-03-23 12:07:08 +0600

    fix: update allowed routes to use the correct port numbers

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -53,6 +53,6 @@ export function isPublicRoute(url: string, method: string) {
 export const ALLOWED_ROUTES: string[] = [
   'http://localhost:3005',
   'http://localhost:3000',
-  'http://192.168.10.58:4046',
-  'http://103.147.163.46:4046',
+  'http://192.168.10.58:4056',
+  'http://103.147.163.46:4056',
 ];
